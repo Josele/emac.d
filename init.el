bfd3d8a2e56e4756e74c7178e77af8f2de404dab
jlm <jl.moragas@hotmail.es>
2021-05-29 20:04:30 +0200

    addding vim powerline with compatibility for evil, adding some c/c++ coding style rules

--- init.el
+++ init.el
@@ -20,6 +20,7 @@
 ;; Initialize use-package on non-Linux platforms
 (unless (package-installed-p 'use-package)
   (package-install 'use-package))
+
 ;; Install and enable packages with use-package
 (require 'use-package)
 (setq use-package-always-ensure t)
@@ -30,10 +31,23 @@
 
 (which-key-mode)
 
+;;Powerline import from submodules
+;;https://www.emacswiki.org/emacs/powerline.el
+(add-to-list 'load-path "~/.emacs.d/vendor/emacs-powerline")
+(require 'powerline)
+;;Utilities for better Evil support for Powerline
+;;https://github.com/johnson-christopher/powerline-evil.
+(add-to-list 'load-path "~/.emacs.d/vendor/powerline-evil")
+(require 'powerline-evil)
+(powerline-evil-vim-color-theme)
+
 ;; Getting evil and bind to key
 (use-package evil
 	     :ensure t)
 (global-set-key (kbd "<f12>") 'evil-mode)
+
+;; Tabs to 4
+(setq-default tab-width 4)
 
 ;; Other bindings
 (global-set-key [f1] 'compare-windows)
@@ -51,7 +65,46 @@
     ))
 
 
-;; Experimental features
+;; Experimental feiatures
+;; Emacs as a C++ IDE
+(setq c-default-style "linux"
+      c-basic-offset 4)
+(defun compact-format ()
+  "Set tabulation with 4 spaces"
+  (interactive)
+  (save-excursion
+    (setq tab-width 4)
+    (setq-default indent-tabs-mode nil)
+    (setq c-basic-offset 4)))
+
+(defun linux-format ()
+  "Set tabulation with tabulation character"
+  (interactive)
+  (save-excursion
+    (setq tab-width 8)
+    (setq-default indent-tabs-mode t)
+    (setq c-basic-offset 8)))
+;;(defun my-c-indent-setup ()
+;;  (setq c-basic-offset 4)
+;;  (setq indent-tabs-mode nil)
+;;  (setq-default fill-column 80))
+(add-hook 'c-mode-common-hook 'linum-mode)
+(add-hook 'c-mode-common-hook '(lambda () (c-toggle-auto-hungry-state 1)))
+(add-hook 'c-mode-common-hook '(lambda () (c-toggle-auto-newline 1)))
+
+;;(add-hook 'c-mode-common-hook 'compact-format)
+;; ;;Projectile: This library provides easy project management and navigation
+(require 'subr-x)
+(use-package projectile
+  :ensure t
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+	      ("s-p" . projectile-command-map)
+              ("C-x p" . projectile-command-map)))
+
+(setq projectile-project-search-path '("~/Source/cpp_workarea/"))
+
 ;; lsp-mode for using emacs as IDE
 (defun efs/lsp-mode-setup ()
   (setq lsp-headerline-breadcrumb-segments '(path-up-to-project file symbols))
@@ -88,7 +141,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (python-mode lsp-mode evil-escape which-key use-package evil))))
+    (projectile company python-mode lsp-mode evil-escape which-key use-package evil))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
